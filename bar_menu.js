d7a488e63006326264d74d8d07351cc38a5f29c5
Peter Zorzonello <zorzonp@bu.edu>
2020-12-18 08:16:35 -0500

    added 'X' animation to menu

--- bar_menu.js
+++ bar_menu.js
@@ -1,4 +1,22 @@
 
+//for the animation of the burger to 'X'
+const menuBtn = document.querySelector('.menu_button');
+const menuList = document.querySelector('.test');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuList.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuList.classList.remove('open');
+        menuOpen = false;
+    }
+});
+
+
+
 function menu(){
   
   var coll = document.getElementsByClassName("collapsible");
