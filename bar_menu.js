a9f04f708014bb74d6cdfca73e92d47dcd11c2ea
Peter Zorzonello <zorzonp@bu.edu>
2020-12-18 19:53:34 -0500

    fixed the menu and added color

--- bar_menu.js
+++ bar_menu.js
@@ -1,37 +1,23 @@
 
 //for the animation of the burger to 'X'
 const menuBtn = document.querySelector('.menu_button');
-const menuList = document.querySelector('.test');
+const menuAboutMe = document.querySelector('.menu_1');
+const menuEducation = document.querySelector('.menu_2');
+const menuCareer = document.querySelector('.menu_3');
+
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
         menuBtn.classList.add('open');
-        menuList.classList.add('open');
+        menuAboutMe.classList.add('open');
+        menuEducation.classList.add('open');
+        menuCareer.classList.add('open');
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
-        menuList.classList.remove('open');
+        menuAboutMe.classList.remove('open');
+        menuEducation.classList.remove('open');
+        menuCareer.classList.remove('open');
         menuOpen = false;
     }
 });
-
-
-
-function menu(){
-  
-  var coll = document.getElementsByClassName("collapsible");
-  var i;
-
-  for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if (content.style.display === "block") {
-        content.style.display = "none";
-      } else {
-        content.style.display = "block";
-      }
-    });
-  }
-}
-
